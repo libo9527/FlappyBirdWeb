29c04864382a3baa889535a4d21f2c6406b80952
libo <18629374709@163.com>
2018-08-27 20:15:29 +0800

    canvas图片加载

--- Main.js
+++ Main.js
@@ -2,14 +2,32 @@
 import {ResoucesLoader} from "./js/base/ResoucesLoader.js";
 
 export class Main {
-    constructor(){
+    constructor() {
         this.canvas = document.getElementById('game_canvas');
         this.ctx = this.canvas.getContext('2d');
         const loader = ResoucesLoader.create();
-        loader.onloaded(map => this.onResourceFirstLoaded(map))
+        loader.onloaded(map => this.onResourceFirstLoaded(map));
+
+        let image = new Image();
+        image.src = '../res/background.png';
+
+        image.onload = () => {
+            this.ctx.drawImage(
+                image,
+                0,
+                0,
+                image.width,
+                image.height,
+                0,
+                0,
+                image.width,
+                image.height
+            );
+        };
+
     }
 
-    onResourceFirstLoaded(map){
+    onResourceFirstLoaded(map) {
         console.log(map);
     }
 }
