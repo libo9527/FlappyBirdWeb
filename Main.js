870dc3355da90a284500a393a862095bcb71defa
libo <18629374709@163.com>
2018-08-27 22:58:04 +0800

    4-8 canvas运动渲染不断移动的地板

--- Main.js
+++ Main.js
@@ -1,33 +1,29 @@
 // 初始化整个游戏的精灵，作为游戏开始的入口
 import {ResoucesLoader} from "./js/base/ResoucesLoader.js";
+import {BackGround} from "./js/runtime/BackGround.js";
+import {DataStore} from "./js/base/DataStore.js";
+import {Director} from "./js/Director.js";
+import {Land} from "./js/runtime/Land.js";
 
 export class Main {
     constructor() {
         this.canvas = document.getElementById('game_canvas');
         this.ctx = this.canvas.getContext('2d');
+        this.dataStore = DataStore.getInstance();
         const loader = ResoucesLoader.create();
         loader.onloaded(map => this.onResourceFirstLoaded(map));
-
-        let image = new Image();
-        image.src = '../res/background.png';
-
-        image.onload = () => {
-            this.ctx.drawImage(
-                image,
-                0,
-                0,
-                image.width,
-                image.height,
-                0,
-                0,
-                image.width,
-                image.height
-            );
-        };
-
     }
 
     onResourceFirstLoaded(map) {
-        console.log(map);
+        this.dataStore.ctx = this.ctx;
+        this.dataStore.res = map;
+        this.init();
+    }
+
+    init() {
+        this.dataStore
+            .put('background', BackGround)
+            .put('land',Land);
+        Director.getInstance().run();
     }
 }
--- js/Director.js
+++ js/Director.js
@@ -1,13 +1,24 @@
 // 导演类，控制游戏的逻辑
-export class Director {
-    constructor(){
-        console.log('构造器初始化');
-    }
+import {DataStore} from "./base/DataStore.js";
 
-    static getInstance(){
-        if (!Director.instance){
+export class Director {
+
+    static getInstance() {
+        if (!Director.instance) {
             Director.instance = new Director();
         }
         return Director.instance;
+    }
+
+    constructor() {
+        this.dataStore = DataStore.getInstance();
+    }
+
+    run() {
+        this.dataStore.get('background').draw();
+        this.dataStore.get('land').draw();
+        let timer = requestAnimationFrame(() => this.run());
+        this.dataStore.put('timer', timer);
+        // cancelAnimationFrame(this.dataStore.getImage('timer'));
     }
 }
